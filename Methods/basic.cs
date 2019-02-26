bc8a0e2e30a4de8ca98184ba98e2ec3b76fb1201
Piyush <bhangalepiyush@gmail.com>
2019-02-26 19:23:50 +0530

    Comments?

--- Methods/basic.cs
+++ Methods/basic.cs
@@ -1,15 +1,22 @@
+/* use the modules used by the program */
 using System;
 
+/* Set The Namespace For program */
 namespace iona
 {
+    /* Class Initialization */
     class iona
     {
+        /* The method / Function. Whatever You call em  */
         static void sayHello(){
             Console.WriteLine("[ sayHello ] Hello World");
         }
 
+        /* The Main Function */
         static void Main(string[] args){
             sayHello();
         }
     }
 }
+
+/* Outputs:  https://images.ionadev.ml/i/S16MJB.png*/
