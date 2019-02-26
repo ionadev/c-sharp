cc03b309af45b2ede45af66536cdb42f35f1e250
Piyush <bhangalepiyush@gmail.com>
2019-02-26 19:01:04 +0530

    Methods / Functions

---
+++ Methods/basic.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace iona
+{
+    class iona
+    {
+        static void sayHello(){
+            Console.WriteLine("[ sayHello ] Hello World");
+        }
+
+        static void Main(string[] args){
+            sayHello();
+        }
+    }
+}
