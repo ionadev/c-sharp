22087ed7114b84ceaa63f3acb18edc3f9c1d950b
Piyush <bhangalepiyush@gmail.com>
2019-02-26 18:46:05 +0530

    Wasnt This For Loop?

--- Arrays/for_loop/for_loop.cs
+++ Arrays/for_loop/for_loop.cs
@@ -6,7 +6,12 @@ namespace iona
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("iona");
+            int[] numbers = new int[10] {1,2,3,4,5,6,7,8,9,0};
+
+            for(int i=0;i<10;i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
         }
     }
 }
